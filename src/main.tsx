fee949c2f8237ef988d4ca09523f50d7fe42732a
Bshaarr <bsharabomorad0@gmail.com>
2026-09-12 11:04:20 +0300

    Initialize main.tsx with React setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,10 @@
-// src/main.tsx placeholder
+import React from 'react';
+import ReactDOM from 'react me/client';
+import App from './App';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
